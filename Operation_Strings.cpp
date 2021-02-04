cd060fbf982ec363c6364ae04e54c8fb36b42862
Sandeep Bhatia <sabhatia@gmail.com>
2021-02-04 14:08:50 -0800

    Added Uppercase ability

--- Operation_Strings.cpp
+++ Operation_Strings.cpp
@@ -4,22 +4,29 @@
 using namespace std;
 #define MAX_STR_SIZE 80
 
-void string_tolower(string &in_str, string &lower_str)
+void string_tolower(string &in_str, string &buffer_str)
 {
     for (int indx=0; indx < in_str.length(); indx++)
     {
-        lower_str[indx] = (char)tolower((char)in_str[indx]);
+        buffer_str[indx] = (char)tolower((char)in_str[indx]);
     }
 }
 
+void string_toupper(string &in_str, string &buffer_str)
+{
+    for (int indx=0; indx < in_str.length(); indx++)
+    {
+        buffer_str[indx] = (char)toupper((char)in_str[indx]);
+    }
+}
 int main()
 {
 
     string user_str;
-    string lower_str;
+    string buffer_str;
 
     user_str.resize(MAX_STR_SIZE);
-    lower_str.resize(MAX_STR_SIZE);
+    buffer_str.resize(MAX_STR_SIZE);
 
     cout << "Enter a string: ";
     getline(cin, user_str);
@@ -29,6 +36,10 @@ int main()
     cout << "Length: " << user_str.length() << endl;
 
     // String Lower
-    string_tolower(user_str, lower_str);
-    cout << "Lowercase: " << lower_str << endl;
+    string_tolower(user_str, buffer_str);
+    cout << "Lowercase: " << buffer_str << endl;
+     
+    // String Upper
+    string_toupper(user_str, buffer_str);
+    cout << "Uppercase: " << buffer_str << endl;
 }
