c6b8a047bb05943506aabfc9fc96b5e287b4aee6
Sandeep Bhatia <sabhatia@gmail.com>
2021-02-04 13:59:20 -0800

    Added lowercase function

--- Operation_Strings.cpp
+++ Operation_Strings.cpp
@@ -4,18 +4,31 @@
 using namespace std;
 #define MAX_STR_SIZE 80
 
+void string_tolower(string &in_str, string &lower_str)
+{
+    for (int indx=0; indx < in_str.length(); indx++)
+    {
+        lower_str[indx] = (char)tolower((char)in_str[indx]);
+    }
+}
+
 int main()
 {
 
     string user_str;
-    user_str.reserve(MAX_STR_SIZE);
+    string lower_str;
 
-    //char user_buf[80];
+    user_str.resize(MAX_STR_SIZE);
+    lower_str.resize(MAX_STR_SIZE);
 
     cout << "Enter a string: ";
     getline(cin, user_str);
     cout << "You entered: " << user_str << endl;
 
-    // String Operations
-    cout << "String Length: " << user_str.length() << endl;
+    // String Length
+    cout << "Length: " << user_str.length() << endl;
+
+    // String Lower
+    string_tolower(user_str, lower_str);
+    cout << "Lowercase: " << lower_str << endl;
 }
