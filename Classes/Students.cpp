119715037c81de345d7e50ce20ff11ea576d430f
Sandeep Bhatia <sabhatia@gmail.com>
2021-02-09 23:49:27 -0800

    Filled in other student functions

--- Classes/Students.cpp
+++ Classes/Students.cpp
@@ -7,50 +7,71 @@ class Student
 {
     private:
         const uint TOTAL_SUBJECTS = 3;
-        uint roll_number;
-        string name;
-        uint marks[3];
+        uint    roll_number;
+        string  name;
+        uint*   marks;
     public:
         Student(uint s_roll, string s_name, uint *s_marks);
-        bool get_transcript(uint *marks_buf);
-        uint  get_total_marks();
-        char  get_grade();
-        void  display_details();
+        uint* get_transcript(void);
+        uint  get_total_marks(void);
+        uint  get_total_subjects(void);
+        char  get_grade(void);
+
         friend ostream& operator<<(ostream& out, Student &s1);
 };
 
 Student::Student(uint s_roll, string s_name, uint* s_marks)
 {
+    // deep copy - specially the marks
     this->roll_number = s_roll;
     this->name = s_name;
-    memcpy(this->marks, s_marks, sizeof(this->marks));
+    this->marks = new uint[this->TOTAL_SUBJECTS];
+    memcpy(this->marks, s_marks, sizeof(uint) * this->TOTAL_SUBJECTS);
 }
 
-bool Student::get_transcript (uint* marks_buf)
+uint* Student::get_transcript (void)
 {
-    return true;
+    // Allocate memory for return
+    uint* marks_list = new uint[this->TOTAL_SUBJECTS];
+    if (marks_list == nullptr) 
+    {
+        return nullptr;
+    }
+    memcpy(marks_list, this->marks, sizeof(uint) * this->TOTAL_SUBJECTS);
+    return marks_list;
+}
+uint Student::get_total_subjects (void)
+{
+    return this->TOTAL_SUBJECTS;
 }
 
-uint Student::get_total_marks(void)
+uint Student::get_total_marks (void)
 {
-    return 0;
+    int total_marks = 0;
+    for (int indx = 0; indx < this->TOTAL_SUBJECTS; indx++)
+    {
+        total_marks += this->marks[indx];
+    }
+    return (total_marks);
 }
 
 char Student::get_grade(void)
 {
-    return ('Z');
-}
-
-void Student::display_details(void)
-{
-    cout << "Student Record" << endl;
-    cout << "--------------" << endl;
-    cout << "Name: " << this->name << endl;
-    cout << "Roll #: " << this->roll_number << endl;
-    cout << "Marks:" << endl;
-    for (int indx = 0; indx < this->TOTAL_SUBJECTS; indx++)
+    const uint MIN_B_GRADE = 60;
+    const uint MIN_A_GRADE = 80;
+    
+    float avg_score = this->get_total_marks()/this->TOTAL_SUBJECTS;
+    if (avg_score < MIN_B_GRADE) 
     {
-        cout << "\tSubject[" << indx+1 << "]: " << this->marks[indx] << endl;
+        return 'C';
+    } 
+    else if (avg_score < MIN_A_GRADE)
+    {
+        return 'B';
+    } 
+    else
+    {
+        return 'A';
     }
 }
 
@@ -65,15 +86,35 @@ ostream& operator<< (ostream& cout, Student &s1)
     {
         cout << "\tSubject[" << indx+1 << "]: " << s1.marks[indx] << endl;
     }
+    cout << "--------------" << endl;
     return cout;
 }
 
 int main()
 {
-    uint s1_marks[] = {70,80,90};
+    uint s1_marks[] = {60,70,80};
     string s1_name = "Sandeep";
     Student *s1 = new Student(1, s1_name, s1_marks);
-    s1->display_details();
+
+    // Print the student record
     cout << *s1;
+
+    // Print the marks individually
+    uint *marks_list = s1->get_transcript();
+    cout << endl << "Student Marks" << endl;
+    cout << "--------------" << endl;
+    for (int indx = 0; indx < s1->get_total_subjects(); indx++)
+    {
+        cout << "Subject[" << indx << "]: " << marks_list[indx] << endl;
+    }
+
+    // Print the total of all subjects
+    cout << endl << "Total Marks: " << s1->get_total_marks() << endl;
+
+    // Print the grade
+    cout << endl << "Student Grade: " << s1->get_grade() << endl;
+
+    // Free any memory used
+    delete(marks_list);
     delete(s1);
 }
