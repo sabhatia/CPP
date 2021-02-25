1268ca6202b3fc8e981bb10dea01494fb68e0267
Sandeep Bhatia <sabhatia@gmail.com>
2021-02-25 09:18:23 -0800

    Added exception handling

--- Stacks/Stack_Test.cpp
+++ Stacks/Stack_Test.cpp
@@ -7,10 +7,29 @@ int main()
     cout << "Stack Size: " << s1.get_size() << endl;
     
     cout << "S1(1): " << s1 << endl;
-    s1.push(10); s1.push(20);
+    try {
+        s1.push(10); s1.push(20); s1.push(30); s1.push(40); s1.push(50); s1.push(60); s1.push(70);
+    }
+    catch (u_int stack_exception)
+    {
+        cout << "ERROR (" << stack_exception << "): Could not push element. Stack full" << endl;
+    }
+
+    // Let's make sure we know what the stack looks like
     cout << "S1(2): " << s1 << endl;
-    cout << "S1 Popped: " << s1.pop() << endl;
-    s1.pop(); s1.pop();
+    try {
+        cout << "S1 Popped: " << s1.pop() << endl;
+        s1.pop(); s1.pop();
+        s1.pop(); s1.pop();
+        s1.pop(); s1.pop();
+        s1.pop(); s1.pop();
+    }
+    catch (u_int stack_exception)
+    {
+        cout << "ERROR (" << stack_exception << "): Could not pop element. Stack empty. " << endl;
+    }
+
+    // Let's make sure we know what the stack looks like
     cout << "S1(3): " << s1 << endl;
 }
  
