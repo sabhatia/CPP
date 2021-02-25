79b41069eeb78d435191f54fa487afea2b8564ae
Sandeep Bhatia <sabhatia@gmail.com>
2021-02-24 22:02:04 -0800

    Changed directory structure

--- Stacks/Stack_Exceptions.cpp
+++ Stacks/Stack_Exceptions.cpp
@@ -56,16 +56,3 @@ int Stack::pop (void)
 
     return (0);
 }
-
-int main()
-{
-    Stack s1;
-    cout << "Stack Size: " << s1.get_size() << endl;
-    
-    cout << "S1(1): " << s1 << endl;
-    s1.push(10); s1.push(20);
-    cout << "S1(2): " << s1 << endl;
-    cout << "S1 Popped: " << s1.pop() << endl;
-    s1.pop(); s1.pop();
-    cout << "S1(3): " << s1 << endl;
-}
