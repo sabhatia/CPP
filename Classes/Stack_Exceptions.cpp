99d72e83a52a7e6f0f7e8ac7810df64199d4cd95
Sandeep Bhatia <sabhatia@gmail.com>
2021-02-24 17:43:55 -0800

    added size set and get

--- Classes/Stack_Exceptions.cpp
+++ Classes/Stack_Exceptions.cpp
@@ -12,9 +12,12 @@ class Stack
         static const u_int STACK_SIZE_MAX     = 20;
         static const u_int STACK_SIZE_MIN     = 5;
         u_int stack_size;
+        int *stack_array;
     public:
         Stack(u_int init_stack_size = STACK_SIZE_DEFAULT);
-
+        u_int get_size ();
+        void push (int elem_val);
+        int pop (void);
 };
 
 Stack::Stack (u_int init_stack_size)
@@ -27,9 +30,16 @@ Stack::Stack (u_int init_stack_size)
     {
         this->stack_size = init_stack_size;
     }
+    stack_array = new int[this->stack_size];
+}
+
+u_int Stack::get_size()
+{
+    return (this->stack_size);
 }
 
 int main()
 {
-
+    Stack s1;
+    cout << "Stack Size: " << s1.get_size() << endl;
 }
