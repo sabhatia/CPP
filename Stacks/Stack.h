0886d2289644f725dd588dd85f3737042fc501d5
Sandeep Bhatia <sabhatia@gmail.com>
2021-02-25 09:09:35 -0800

    Renamed base Stack class file

--- Stacks/Stack.h
+++ Stacks/Stack.h
@@ -11,15 +11,19 @@ class Stack
         static const u_int STACK_SIZE_DEFAULT = 5;
         static const u_int STACK_SIZE_MAX     = 20;
         static const u_int STACK_SIZE_MIN     = 5;
-        u_int stack_size;
+
+        // Internal data structure related declarations
         u_int stack_depth = 0;
+        u_int stack_size;
         int* stack_array;   // dynamically allocated array
+
     public:
 
-        //Constants for Error conditions
+        // Constants for Error conditions
         static const u_int STACK_ERROR_OVERFLOW = 101;
         static const u_int STACK_ERROR_UNDERFLOW = 102;
-        
+
+        // Stack API
         Stack(u_int init_stack_size = STACK_SIZE_DEFAULT);
         u_int get_size ();
         void push (int elem_val);
