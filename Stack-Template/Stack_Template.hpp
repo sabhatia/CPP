570ad5f98445c1d5a357a6a207962919a80ddd56
Sandeep Bhatia <sabhatia@gmail.com>
2021-03-02 13:10:41 -0800

    Base Stack Template test class

--- Stack-Template/Stack_Template.hpp
+++ Stack-Template/Stack_Template.hpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
 template <class T>
 
 class Stack_Template
---
+++ Stack-Template/Stack_Template_Test.cpp
@@ -0,0 +1,22 @@
+#include "Stack_Template.hpp"
+
+using namespace std;
+
+int main()
+{
+    Stack_Template s1<int>;
+    Stack_Template s2<float>;
+
+    // TC(1): Basic Push
+    s1.push(10); s1.push(20); s1.push(30);
+    s2.push(11.11); s2.push(22.22); s2.push(33.33);
+    cout << s1; cout << s2;
+
+    // TC(2): Basic Pop
+    s1.pop(); s2.pop();
+    cout << s1; cout << s2;
+
+    s1.pop(); s2.pop();
+    cout << s1; cout << s2;
+
+}
