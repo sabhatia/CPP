5d38ae0fa9b6312ea2bb544fd9ee4c4809cc4a7f
Sandeep Bhatia <sabhatia@gmail.com>
2021-03-06 12:05:59 -0800

    Added a Banking_System class

--- Banking-System/Banking_System.cpp
+++ Banking-System/Banking_System.cpp
@@ -1,40 +1,39 @@
-#include "Bank_Account_Test.cpp"
+#ifndef BANKING_SYSTEM_CPP_
+#define BANKING_SYSTEM_CPP_
+#include <vector>
+#include "Bank_Account.cpp"
 
-void print_banking_banner()
+class Banking_System
 {
-    cout << "=====================================================" << endl;
-    cout << "\t\tWelcome to Sandeep Bank" << endl;
-    cout << "=====================================================" << endl;
-}
+private:
+    vector<Bank_Account> accounts_vct;
+public:
+    static void print_banking_banner()
+    {
+        cout << "=====================================================" << endl;
+        cout << "\t\tWelcome to Sandeep Bank" << endl;
+        cout << "=====================================================" << endl;
+    }
 
-void print_banking_menu()
-{
-    cout << "Select from the following options:" << endl;
-    cout << "1: Open  an account" << endl;
-    cout << "2: Close an account" << endl;
-    cout << "3: Show all account(s)" << endl;
-    cout << "4: Account Balance Inquiry" << endl;
-    cout << "5: Deposit  funds" << endl;
-    cout << "6: Withdraw funds" << endl;
-    cout << "7: Quit" << endl; 
-}
+    static void print_banking_menu()
+    {
+        cout << "Select from the following options:" << endl;
+        cout << "1: Open  an account" << endl;
+        cout << "2: Close an account" << endl;
+        cout << "3: Show all account(s)" << endl;
+        cout << "4: Account Balance Inquiry" << endl;
+        cout << "5: Deposit  funds" << endl;
+        cout << "6: Withdraw funds" << endl;
+        cout << "7: Quit" << endl; 
+    }
 
-u_int get_banking_option()
+    static u_int get_banking_option()
+    {
+        u_int selected_banking_option;
+        cout << "Your selection: ";
+        cin >> selected_banking_option;
+        return (selected_banking_option);
+    }
+};
 
-{
-    u_int selected_banking_option;
-    cout << "Your selection: ";
-    cin >> selected_banking_option;
-    return (selected_banking_option);
-}
-
-int main()
-{
-    u_int banking_option = 0;
-
-    print_banking_banner();
-    print_banking_menu();
-    banking_option = get_banking_option();
-    cout << "Selected Option: " << banking_option << endl;
-
-}
+#endif /* BANKING_SYSTEM_CPP_ */
---
+++ Banking-System/Banking_System_Test.cpp
@@ -0,0 +1,16 @@
+#include "Bank_Account_Test.cpp"
+#include "Banking_System.cpp"
+
+int main()
+{
+    u_int banking_option = 0;
+
+    // Initialize the Ban
+
+    // Print the starting banner and options
+    Banking_System::print_banking_banner();
+    Banking_System::print_banking_menu();
+    banking_option = Banking_System::get_banking_option();
+    cout << "Selected Option: " << banking_option << endl;
+
+} 
