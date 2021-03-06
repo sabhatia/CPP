2772de06d6d6513c2c9e51dc431225b00d9f5853
Sandeep Bhatia <sabhatia@gmail.com>
2021-03-06 15:55:26 -0800

    Bank-DB: Added << function

--- Banking-System/Banking_DB.cpp
+++ Banking-System/Banking_DB.cpp
@@ -3,6 +3,9 @@
 
 #include <string>
 #include <fstream>
+#include <vector>
+#include <cassert>
+#include "Bank_Account.cpp"
 
 using namespace std;
 
@@ -11,10 +14,28 @@ class Banking_DB
 private:
     string data_source_name;
     fstream data_file;
+    vector<Bank_Account> *accounts_vct; // make this shared ptr later
 public:
-    Banking_DB(string data_source)
+    Banking_DB(string data_source, vector<Bank_Account> *ba_vct)
     {
         this->data_source_name = data_source;
+        assert(ba_vct != nullptr);
+        this->accounts_vct = ba_vct; // validate for null pointer
+    }
+
+    friend ostream &operator<<(ostream &console, Banking_DB &db_info)
+    {
+        console << "Bank DB Details:" << endl;
+        console << "\tSource: " << db_info.data_source_name << endl;
+        console << "\tConnection: " << (db_info.data_file.is_open() ? "Open" : "Closed") << endl;
+        console << "\tAccount Details(" << db_info.accounts_vct->size() << ") accounts: " << endl;
+        for (vector<Bank_Account>::iterator itr = db_info.accounts_vct->begin();
+             itr != db_info.accounts_vct->end(); itr++)
+        {
+            console << *itr;
+        }
+
+        return (console);
     }
 };
 #endif /* BANKING_DB_CPP_ */
--- Banking-System/Banking_DB_Test.cpp
+++ Banking-System/Banking_DB_Test.cpp
@@ -5,6 +5,8 @@
 
 int main()
 {
-    Banking_DB file_db("./Test-DB/Test-file.txt");
+    vector<Bank_Account> test_accounts_vct;    
+    Banking_DB file_db("./Test-DB/Test-file.txt", &test_accounts_vct);
+    cout << file_db;
 }
 #endif /* BANKING_DB_TEST_CPP_ */
