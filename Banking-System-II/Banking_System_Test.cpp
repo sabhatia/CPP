cc421cee7764616a621d7ca6e24859e7d9508e8c
Sandeep Bhatia <sabhatia@gmail.com>
2021-03-09 16:45:45 -0800

    Initial check in for google test

---
+++ Banking-System-II/Banking_System_Test.cpp
@@ -0,0 +1,24 @@
+#include "Banking_System.cpp"
+
+int main()
+{
+    BANKING_OPTIONS banking_option = SB_OPTION_NONE;
+
+    // Initialize the Banking system
+    Banking_System sandeep_bank("Sandeep Bank", "./Test-DB/sandeep-bank.txt");
+    // Print the starting banner and options
+    while (true)
+    {
+        /* Bank processing loop */
+        Banking_System::print_banking_banner();
+        Banking_System::print_banking_menu();
+        banking_option = Banking_System::get_banking_option();
+        if (banking_option == SB_OPTION_QUIT_BANKING)
+        {
+            break;
+        }
+        sandeep_bank.process_option(banking_option);
+    }
+
+    cout << "Thank you for banking with " << sandeep_bank.get_name() << endl;
+} 
