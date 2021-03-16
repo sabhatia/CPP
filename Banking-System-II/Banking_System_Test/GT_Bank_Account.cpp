dcddf7e098142fabfc02f0f5f27f8f2a6a4d7b72
Sandeep Bhatia <sabhatia@gmail.com>
2021-03-16 13:44:54 -0700

    Added GTests for Bank_Account

--- Banking-System-II/Banking_System_Test/GT_Bank_Account.cpp
+++ Banking-System-II/Banking_System_Test/GT_Bank_Account.cpp
@@ -1,16 +1,49 @@
 #include <gtest/gtest.h>
+#include "../Banking_DB/Bank_Account.cpp"
 
-TEST(GT_Bank_Account, Test_Macro_TRUE)
+TEST(GTBankAccount, BASetup)
 {
-    EXPECT_TRUE(true);
+    Bank_Account ba1(1, 1, 1000.00, true);
+    EXPECT_EQ(1, ba1.get_account_id());
+    EXPECT_EQ(1, ba1.get_owner_id());
+    EXPECT_EQ(1000.00, ba1.get_funds());
+    EXPECT_EQ(true, ba1.is_open_account());
 }
 
-TEST(GT_Bank_Account, Test_Macro_EQ1)
+TEST(GTBankAccount, BAStoreRetrive)
 {
-    EXPECT_EQ(true, false);
+    Bank_Account ba2(1,1,1000.00, true);
+    string ba_file = "accounts_data.txt";
+    ofstream out_file(ba_file);
+    out_file << ba2;
+    out_file.close();
+
+    ifstream in_file(ba_file);
+    Bank_Account ba3;
+    in_file >> ba3;
+
+    EXPECT_EQ(ba2.get_account_id(), ba3.get_account_id());
+    EXPECT_EQ(ba2.get_owner_id(), ba3.get_owner_id());
+    EXPECT_EQ(ba2.get_funds(), ba3.get_funds());
+    EXPECT_EQ(ba2.is_open_account(), ba3.is_open_account());
 }
 
-TEST(GT_Bank_Account, Test_Macro_EQ2)
+TEST(GTBankAccount, BADeposit)
 {
-    EXPECT_EQ(true, true);
+    float origin_amt  = 4000.00;
+    float deposit_amt = 44.78;
+    Bank_Account ba4(4, 40, origin_amt, true);
+    ba4.deposit_funds(deposit_amt);
+
+    EXPECT_EQ(origin_amt + deposit_amt, ba4.get_funds());
+}
+
+TEST(GTBankAccount, BAWithdraw)
+{
+    float origin_amt = 4567.89;
+    float remove_amt =  567.89;
+    Bank_Account ba5(5, 50, origin_amt, true);
+    ba5.withdraw_funds(remove_amt);
+    
+    EXPECT_EQ(origin_amt - remove_amt, ba5.get_funds());
 }
